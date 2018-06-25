a913591040650e66546f97fba31b3a7e6cab1bdc
chrisKingsley <kingsley.chris@gmail.com>
2018-06-25 00:05:59 -0700

    updates to PointSET

--- assignment 5/code/PointSET.java
+++ assignment 5/code/PointSET.java
@@ -1,22 +1,27 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 public class PointSET {
-	int numPoints;
+	TreeSet<Point2D> tree;
 	
 	// construct an empty set of points
-	public PointSET() {}
+	public PointSET() {
+		tree = new TreeSet<Point2D>();
+	}
 
 	// is the set empty?
 	public boolean isEmpty() {
-		return numPoints == 0;
+		return tree.size() == 0;
 	}
 	
 	// number of points in the set
 	public int size() {
-		return numPoints;
+		return tree.size();
 	}
 
 	// add the point p to the set (if it is not already in the set)
 	public void insert(Point2D p) {
-		
+		tree.add(p);
 	}
 
 	// does the set contain the point p?
@@ -28,14 +33,42 @@ public class PointSET {
 	public void draw() {
 		
 	}
-
-	// all points in the set that are inside the rectangle
+	
+	
+	/**
+	 * all points in the set that are inside the rectangle
+	 * @param rect RectHV object that defines the rectangle
+	 * @return ArrayList of points inside the rectangle
+	 */
 	public Iterable<Point2D> range(RectHV rect) {
+		ArrayList<Point2D> insidePoints = new ArrayList<Point2D>();
 		
+		for (Point2D storedPoint : tree) {
+			if (rect.contains(storedPoint)) {
+				insidePoints.add(storedPoint);
+			}
+		}
+		
+		return insidePoints;
 	}
 
-	// a nearest neighbor in the set to p; null if set is empty
+	/**
+	 * a nearest neighbor in the set to p; null if set is empty
+	 * @param p
+	 * @return
+	 */
 	public Point2D nearest(Point2D p) {
+		Point2D closestPoint = null;
+		double closestDist = Double.MAX_VALUE;
 		
+		for (Point2D storedPoint : tree) {
+			double dist = p.distanceTo(storedPoint);
+			if (dist < closestDist) {
+				closestPoint = storedPoint;
+				closestDist = dist;
+			}
+		}
+		
+		return closestPoint;
 	}
 }
